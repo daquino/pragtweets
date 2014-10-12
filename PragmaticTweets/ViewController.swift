2412741cee56e066a83a69581a143ef492de85ec
Daniel Aquino <daniel@egovtn.com>
2014-10-12 15:49:34 -0500

    Finish chapter 3

--- PragmaticTweets/ViewController.swift
+++ PragmaticTweets/ViewController.swift
@@ -9,22 +9,35 @@
 import UIKit
 import Social
 
-class ViewController: UIViewController {
+public class ViewController: UIViewController {
 
-    override func viewDidLoad() {
+    @IBOutlet public weak var twitterWebView: UIWebView!
+    public override func viewDidLoad() {
         super.viewDidLoad()
+        self.reloadTweets()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
+    public override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func handleShowMyTweetsTapped(sender: UIButton) {
+        self.reloadTweets()
+    }
+    
+    func reloadTweets() {
+        let url = NSURL(string: "http://www.twitter.com/yomilayer3")
+        let urlRequest = NSURLRequest(URL: url)
+        self.twitterWebView.loadRequest(urlRequest)
+    }
+    
     @IBAction func handleTweetButtonTapped(sender: UIButton) {
         if SLComposeViewController.isAvailableForServiceType(SLServiceTypeTwitter) {
+            let message = NSBundle.mainBundle().localizedStringForKey("I just finished the first project in iOS 8 SDK Development. #pragios8", value: "", table: nil)
             let tweetVC = SLComposeViewController (forServiceType: SLServiceTypeTwitter)
-            tweetVC.setInitialText("I just finished the first project in iOS 8 SDK Development. #pragsios8")
+            tweetVC.setInitialText(message)
             self.presentViewController(tweetVC, animated: true, completion: nil)
         }
         else {
